38be61b62fbdd9bc7f765c861ac77313743c8fac
aaronhill <aaron.hill@clearlink.com>
2017-06-26 10:07:22 -0600

    Added a challenge test case.

--- tests/KataTest.php
+++ tests/KataTest.php
@@ -12,4 +12,11 @@ class KataTest extends \PHPUnit_Framework_TestCase
         $val = $kata->calc('1+1');
         $this->assertEquals($val, 2);
     }
+
+    public function test_pemdas()
+    {
+        $kata = new StringCalculator();
+        $val = $kata->calc('(1+3)^2 * (5-12)^3');
+        $this->assertEquals($val, 2);
+    }
 }
